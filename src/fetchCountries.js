710305cd1916d8c2bfd46450fd5b57334193c35c
Mikhail-Ropii <mr.ropiy@gmail.com>
2022-01-09 15:49:21 +0200

    update

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+import Notiflix from 'notiflix';
+export default function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+})
+        // .then(data => data)
+        .catch(() => {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+            })
+    }
+    
